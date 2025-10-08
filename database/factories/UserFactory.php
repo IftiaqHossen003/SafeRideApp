d666fe2d2f0c878b882c2718ce7cbc8652e9a880
IftiaqHossen003 <hossen2107003@stud.kuet.ac.bd>
2025-10-08 16:27:33 +0600

    Add pseudonym & is_volunteer to users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,8 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'pseudonym' => fake()->optional(0.7)->userName(),
+            'is_volunteer' => fake()->boolean(30),
         ];
     }
 
